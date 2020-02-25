7e42737ed6511c1ac86e413c3632450b7bc323cc
dealer426 <burnssamuel01@gmail.com>
2020-02-25 08:43:15 -0500

    Added html labels

--- BlogProject/Migrations/20200225133415_Added Display Type To Blog Model.Designer.cs
+++ BlogProject/Migrations/20200225133415_Added Display Type To Blog Model.Designer.cs
@@ -4,14 +4,16 @@ using BlogProject;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlogProject.Migrations
 {
     [DbContext(typeof(BlogContext))]
-    partial class BlogContextModelSnapshot : ModelSnapshot
+    [Migration("20200225133415_Added Display Type To Blog Model")]
+    partial class AddedDisplayTypeToBlogModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BlogProject/Models/Blog.cs
+++ BlogProject/Models/Blog.cs
@@ -13,15 +13,21 @@ namespace BlogProject.Models
 
         public int BlogId { get; set; }
         [Required(ErrorMessage = "You must enter a Title!")]
+        [Display(Name = "Title")]
         public string Title { get; set; }
         [Required(ErrorMessage = "You must enter Content!")]
+        [Display(Name = "Content")]
         public string Content { get; set; }
         [Required(ErrorMessage = "You must add an Author!")]
+        [Display(Name = "Author")]
         public string Author { get; set; }
+        [Display(Name = "Publish Date")]
         public DateTime PublishDate { get; set; }
         [Required(ErrorMessage = "You must add a Category!")]
+        [Display(Name = "Category")]
         public string Category { get; set; }
         [Required(ErrorMessage = "You must add a Tag!")]
+        [Display(Name = "Tags")]
         public string Tags { get; set; }
 
         //Constructors
