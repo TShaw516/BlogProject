7fc4cea63c7f532db57dc3b6f1e257aeb37e8c3f
dealer426 <burnssamuel01@gmail.com>
2020-02-27 15:58:22 -0500

    Add cat repo to startup

---
+++ BlogProject/Repositories/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using BlogProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Repositories
+{
+    public class CategoryRepository : Repository<Category>
+    {
+        public CategoryRepository(BlogContext context) : base(context)
+        {
+            // This ensures our base constructor gets run.
+        }
+    }
+}
