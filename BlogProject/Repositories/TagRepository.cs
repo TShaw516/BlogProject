53950a3b4bb337885ff5c162c757b78607f2619b
TShaw516 <TShaw516@gmail.com>
2020-02-27 14:21:58 -0500

    Built out tag controller and tag views

---
+++ BlogProject/Repositories/TagRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogProject.Models;
+
+namespace BlogProject.Repositories
+{
+    public class TagRepository : Repository<Tag>
+    {
+        public TagRepository(BlogContext context) : base(context)
+        {
+            // This ensures our base constructor gets run.
+        }
+    }
+}
