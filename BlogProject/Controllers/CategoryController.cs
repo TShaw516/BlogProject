3a272e52654d141a920c7fc34123392fc83917f8
dealer426 <burnssamuel01@gmail.com>
2020-02-27 16:02:11 -0500

    added methods to controller

--- BlogProject/Controllers/CategoryController.cs
+++ BlogProject/Controllers/CategoryController.cs
@@ -2,15 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogProject.Models;
+using BlogProject.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogProject.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        IRepository<Category> myCategories;
+
+        public CategoryController(IRepository<Category> myCategories)
+        {
+            this.myCategories = myCategories;
+        }
+
+        // GET: /<controller>/
+        public ActionResult Index()
+        {
+            var model = myCategories.GetAll();
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var model = myCategories.GetById(id);
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            myCategories.Create(category);
+            return RedirectToAction("Index");
+        }
     }
+}
 }
